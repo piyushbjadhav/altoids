ac31d08bdba730d0027bc5ba1a32be7aa231df6c
Piyush Jadhav <piyush.jadhav@nyu.edu>
2015-04-18 16:42:45 -0400

    Initial Version

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var logger = require('morgan');
+app.use(express.static('public'));
+
+app.use(logger());
+app.get('/event', function (req,res){
+  
+  var ev = {
+  	title:'Coldplay Concert',
+  	venue:'Barclays Center',
+  	city: 'New York'
+  };
+
+  res.json(ev);
+});
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+});
