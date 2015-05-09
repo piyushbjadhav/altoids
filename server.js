36d4ac21e3e2ec2bb09c9c42398804e0cac17892
Piyush Jadhav <piyush.jadhav@nyu.edu>
2015-05-09 19:35:35 -0400

    added POST event ajax

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var eventful = require('eventful-node');
 var client = new eventful.Client('XCQB6Mpg3vXPMFJ7');
 app.use(express.static('public'));
 app.use(logger());
+var bodyParser = require("body-parser");
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/events', function (req,res){
 	client.searchEvents({ keywords: 'music' }, function(err, data){
 	    if(err){
@@ -24,6 +28,13 @@ app.get('/events', function (req,res){
 	    }  
 	   res.json(evlist);
 	});
+});
+
+
+app.post('/putevent',function (req,res){
+
+	console.log(req.body);
+	
 });
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
