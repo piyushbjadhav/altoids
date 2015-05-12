d17fe92cf9f9570266bcad1c45b4cad1597ca6da
shashankg08 <ssg383@nyu.edu>
2015-05-12 13:14:14 -0400

    Kafka integration completed. Added url mapping for backend calls. Need to tweak mongod. #Shashank

---
+++ backend/api.js
@@ -0,0 +1,15 @@
+module.exports = function(){
+
+	//console.log("Initialized / loaded api.js module");
+
+	return {
+		print: function(){
+			console.log("Hi from api.js");
+		},
+
+		hi: function(){
+			console.log("HI from second function");
+		}
+	};
+
+};
