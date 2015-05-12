5c0bef4658c1274918396e1317c5b117e0a464f3
shashankg08 <ssg383@nyu.edu>
2015-05-12 13:18:38 -0400

    #clean up

--- backend1/index.js
+++
@@ -1,9 +0,0 @@
-
-var api = require('./api.js');	// set up links to functions, modulues.
-
-//api();							// actual function call;
-
-var p = api();						// construct an instance / object of module 			
-
-p.print();							// function calls.
-p.hi();
